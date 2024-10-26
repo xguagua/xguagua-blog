2d2c3f436fd9c8878f87205dbdc92ea025619792
xguagua <bilixguagua@outlook.com>
2024-10-26 23:33:29 +0800

    upd:24/10/26

--- src/consts.ts
+++ src/consts.ts
@@ -2,14 +2,14 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_TAB = "Frosti";
+export const SITE_TAB = "Blog of xguagua";
 export const SITE_TITLE = "Frosti 🧊";
 export const SITE_DESCRIPTION = "A blog template";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"
+export const USER_NAME = "xguagua";
+export const USER_SITE = "https://xguagua.cfd"
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
@@ -25,29 +25,6 @@ export const menuItems = [
     href: "/blog",
     svg: "blog",
     target: "_self",
-    subItems: [
-      {
-        id: "all",
-        text: "All blogs",
-        href: "/blog",
-        svg: "post",
-        target: "_self",
-      }, // All blog
-      {
-        id: "tech",
-        text: "Tech blogs",
-        href: "/blog/categories/tech",
-        svg: "cube",
-        target: "_self",
-      }, // Technology category
-      {
-        id: "life",
-        text: "Life blogs",
-        href: "/blog/categories/life",
-        svg: "heart",
-        target: "_self",
-      }, // Life category
-    ],
   }, // Blog page with sub-items
   {
     id: "project",
