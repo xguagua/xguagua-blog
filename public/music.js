b02b267d8561f5ce0e79414eaecd97d02c3a279f
xguagua <bilixguagua@outlook.com>
2024-11-25 21:02:21 +0800

    upd: 11.25

---
+++ public/music.js
@@ -0,0 +1,13 @@
+var videos = ["<iframe src=\"https://player.bilibili.com/player.html?isOutside=true&aid=94997018&bvid=BV1YE411T7aA&cid=365406930&p=1&autoplay=true\"></iframe>",
+];
+
+function rnd()
+{
+    return Math.floor(Math.random() * 114514 / 191 + 1654 * Math.E);
+}
+function logic()
+{
+    console.log(rnd());
+    var display = document.getElementById("displayMain");
+    // display.style = "";
+}
